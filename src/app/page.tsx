3804d3e92c31e2763d4fcd4489b688efe8b94d41
Michael Abiona <amabiona21@gmail.com>
2023-12-21 10:53:20 +0100

    fix add to cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,51 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { publicAxios } from "../utils/axiosConfig";
-import { IPrdoucts } from "@/interface";
+import { IPrdoucts, IProduct } from "@/interface";
 import { ToastMessage } from "@/utils/toast";
 import Link from "next/link";
-import { useAppDispatch } from "@/hook/useRedux";
-import { addToCart } from "@/redux/slices/cart";
+import { useAppDispatch, useAppSelector } from "@/hook/useRedux";
+import { addToCart, setItemQty } from "@/redux/slices/cart";
 
 const Home = () => {
     const dispatch = useAppDispatch();
+    const { cartItems } = useAppSelector(state => state.Cart);
 
     const { data } = useQuery({
 		queryKey: ["products"],
 		queryFn: () => publicAxios.get<IPrdoucts>("/product"),
 	});
 
+    const handleAddToCart = (product: IProduct) => {
+        const productItem = cartItems.find(item => item.id === product.id);
+        const productQtyLessThanStockQty = cartItems.find(item => (item.id === product.id && item.qty < product.store_product_properties[0].stock_quantity));
+
+        if(productItem) {
+            if(productQtyLessThanStockQty) {
+                dispatch(setItemQty({ ...product, qty: productItem.qty + 1 }));
+                ToastMessage("Product added to cart!");
+            }
+        } else {
+            dispatch(addToCart(product));
+            ToastMessage("Product added to cart!");
+        }
+    }
+
     return (
         <div>
-            <div className="container w-11/12 mx-auto flex items-center justify-between py-6 mt-8">
-                <h1 className="text-4xl font-bold">Enlumi Store Front</h1>
-                <Link href={"/cart"} className="flex items-center mr-[250px] cursor-pointer hover:underline">
-                    <p className="mr-1 text-lg">Shopping Cart</p>
+            <div className="container w-11/12 mx-auto flex items-center justify-between py-6 mt-6 md:mt-8">
+                <h1 className="text-xl md:text-4xl font-bold">Enlumi Store Front</h1>
+                <Link href={"/cart"} className="flex items-center md:mr-[250px] cursor-pointer hover:underline">
+                    <p className="mr-1 text-sm md:text-lg">Cart</p>
                     &rarr;
                 </Link>
             </div>
             <hr />
-            <div className="container w-11/12 mx-auto mt-14">
-                <p className="text-xl font-semibold mb-5">All Products</p>
+            <div className="container w-11/12 mx-auto mt-8 md:mt-14 mb-28">
+                <p className="text-lg md:text-xl font-semibold mb-5">All Products</p>
                 <div className="grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     {data && data.data.data.map((product) => {
-                        if(product.store_product_properties[0] && !product.is_service && product.variants.length === 0) {
+                        if(product.store_product_properties[0] && product.store_product_properties[0].stock_quantity > 0 && !product.is_service && product.variants.length === 0) {
                             return (
                                 <div key={product.id} className="w-full">
                                     <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -44,10 +60,7 @@ const Home = () => {
                                             <div className="flex justify-end mt-3">
                                                 <span 
                                                     className="text-xs bg-green-900 px-3 py-2 text-white cursor-pointer"
-                                                    onClick={() => {
-                                                        dispatch(addToCart(product))
-                                                        ToastMessage("Product added to cart!");
-                                                    }}
+                                                    onClick={() => handleAddToCart(product)}
                                                 >
                                                     Add to cart
                                                 </span>
